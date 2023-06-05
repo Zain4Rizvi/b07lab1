f5df28b44e93e2304bbca90f8ad5427ffa3a6b3b
Zain <blackhawksrock9696@gmail.com>
2023-06-04 22:43:11 -0400

    Added Polynomial.java and Driver.java

--- Polynomial.java
+++ Polynomial.java
@@ -1,34 +1,47 @@
 public class Polynomial {
-	double [] coeffecients;
+        double[] coeffecients;
 
-	public void Polynomial(){
-		for (int i = 0; i < coeffecients.length; i++){
-			coeffecients[i] = 0;
-		}
-	}
+        public Polynomial(double [] coeffecient){
+                int l = coeffecient.length;
+                coeffecients = new double[l];
 
-	public void Polynomial(double[] coeffecients){
-		for (int i = 0; i < coeffecients.length; i++){
-			this.coeffecients[i] = coeffecients[i];
-		}
-	}
+                for (int i = 0; i < l; i++){
+                coeffecients[i] = coeffecient[i];
+                }
+        }
 
-	public Polynomial add(Polynomail poly){
-		for (int i = 0; i < poly.length; i++){
-			this.coeffecients[i] = this.coeffecients[i] + poly[i];
-		}
-		return this.coeffecients;
-	}
+        public Polynomial(){
+                coeffecients = new double[]{0};
+        }
 
-	public int evaluate(double x){
-		double ret = 0;
-		for (int i = 0; i < this.coeffecients.length;++){
-			ret = ret + coeffecients[i] * (Math.pow(x,i));
-		}
-		return ret;
-	}
 
-	public boolean hasRoot(double x){
-		return (evaluate(x) == 0);
-	}
-	
+
+        public Polynomial add(Polynomial poly){
+                int l = Math.min(this.coeffecients.length, poly.coeffecients.length);
+                if (poly.coeffecients.length < this.coeffecients.length){
+                        for (int i = 0; i < l; i++){
+                                this.coeffecients[i] = this.coeffecients[i] + poly.coeffecients[i];
+                        }
+                        return this;
+                }
+
+                for (int i = 0; i < l; i++){
+                                poly.coeffecients[i] = this.coeffecients[i] + poly.coeffecients[i];
+                        }
+                        return poly;
+
+        }
+
+        public double evaluate(double x){
+                double ret = 0;
+                for (int i = 0; i < this.coeffecients.length; i++){
+                        ret = ret + coeffecients[i] * (Math.pow(x,i));
+                }
+                return ret;
+        }
+
+        public boolean hasRoot(double x){
+                return (evaluate(x) == 0);
+        }
+
+}
